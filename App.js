8bc6a1e005212451fe8fbccfa50754958d41af59
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-09 23:31:11 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+//import logo from './logo.svg';
+import './App.css';
+import List from './components/List.js'
+//import ButtonToAdd from './components/ButtonToAdd.js'
+//import logo from '.src/shopping.jpg'
+function App() {
+  return (
+
+    <main className='gosho'>
+      <div className='withblur'>
+    <div className="App">
+      <div className = "Logo">
+      </div>
+      <header className="App-header">
+        <h1>Лист за пазаруване</h1>
+        <List/> 
+    <footer>All rights reserved &copy;</footer>
+      </header>
+    </div>
+    </div>
+    </main>
+  );
+}
+
+export default App;
